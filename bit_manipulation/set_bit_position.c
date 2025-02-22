947d8818f8612f9672333551b34cc59fff4aa887
Kanagaraj NN <n.n.kanagaraj.upm@gmail.com>
2025-02-22 08:57:01 +0530

    Update set_bit_position.c

--- bit_manipulation/set_bit_position.c
+++ bit_manipulation/set_bit_position.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-#define SET_BIT(num, pos) ((num) | (1 << pos))
+#define SET_BIT(num, pos) ((num) | (1 << (pos)))
 
 void print_binary(unsigned int num) {
     for (int i = sizeof(num) * 8 - 1; i >= 0; i--) {
         printf("%d", (num >> i) & 1);
     }
+    printf("\n");
 }
 
 int main() {
@@ -14,7 +15,7 @@ int main() {
     print_binary(reg);
     
     reg = SET_BIT(reg, bit_position);   
-    printf("\nAfter setting bit %d: \n", bit_position);
+    printf("After setting bit %d: \n", bit_position);
     
     print_binary(reg);
 }
