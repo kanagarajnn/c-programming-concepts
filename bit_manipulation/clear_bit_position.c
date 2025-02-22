84fc1cf66c4e386b0f4efad08b0ba47cee3b6960
Kanagaraj NN <n.n.kanagaraj.upm@gmail.com>
2025-02-22 08:57:48 +0530

    Update clear_bit_position.c

--- bit_manipulation/clear_bit_position.c
+++ bit_manipulation/clear_bit_position.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#define CLEAR_BIT(num, pos) ((num) & ~(1 << pos))
+#define CLEAR_BIT(num, pos) ((num) & ~(1 << (pos)))
 
 void print_binary(unsigned int num) {
     for (int i = sizeof(num) * 8 - 1; i >= 0; i--) {
         printf("%d", (num >> i) & 1);
     }
+    printf("\n");
 }
 
 int main() {
     int reg = 0b00001110;  // Initial register value (decimal 14)
-    int bit_position = 2;  // We want to set the 2nd bit
+    int bit_position = 2;  // We want to clear the 2nd bit
 
     print_binary(reg);
     
     reg = CLEAR_BIT(reg, bit_position);   
-    printf("\nAfter setting bit %d: \n", bit_position);
+    printf("After clearing bit %d: \n", bit_position);
     
     print_binary(reg);
 }
