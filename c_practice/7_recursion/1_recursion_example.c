d7e147c2d757b5104408e39360ebc695bfeebd76
Kanagaraj NN <n.n.kanagaraj.upm@gmail.com>
2025-03-29 11:48:53 +0530

    Add recursion example code

---
+++ c_practice/7_recursion/1_recursion_example.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int fun(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return 7 + fun(n - 2);
+}
+
+int main() {
+    printf("%d", fun(4));
+    return 0;
+}
+
+/*
+
+fun(4)
+return 7 + fun(2)
+return 7 + fun(0)
+return 1
+
+
+fun(4)
+return 15
+
+
+
+*/
